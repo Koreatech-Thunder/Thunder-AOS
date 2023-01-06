5dd44bd577913b02414490f62e6d3fe6b57c518b
KWY <wodrk1001@gmail.com>
2023-01-06 20:26:22 +0900

    [feat] 번개 뷰 도메인 구현

---
+++ app/src/main/java/com/koreatech/thunder/domain/repository/ThunderRepository.kt
@@ -0,0 +1,22 @@
+package com.koreatech.thunder.domain.repository
+
+import com.koreatech.thunder.domain.entity.HashTag
+import com.koreatech.thunder.domain.entity.Thunder
+import com.koreatech.thunder.domain.entity.User
+
+interface ThunderRepository {
+    suspend fun getThunders(): Result<List<Thunder>>
+    suspend fun getHashTaggedThunders(hashTag: HashTag): Result<List<Thunder>>
+    suspend fun getUser(userId: String): Result<User>
+    suspend fun postThunder(
+        userId: String,
+        title: String,
+        content: String,
+        hashtags: List<HashTag>,
+        limitParticipantsCnt: Int,
+        deadline: String
+    ): Result<Unit>
+
+    suspend fun enterThunder(userId: String): Result<Unit>
+    suspend fun cancelThunder(userId: String): Result<Unit>
+}
