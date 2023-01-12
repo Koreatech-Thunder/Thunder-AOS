63f34fe820dcf647c21e9c74d07e083de699eef6
KWY <wodrk1001@gmail.com>
2023-01-12 23:26:36 +0900

    [feat] tool bar 슬롯 구현

---
+++ app/src/main/java/com/koreatech/thunder/designsystem/components/ThunderToolBarSlot.kt
@@ -0,0 +1,34 @@
+package com.koreatech.thunder.designsystem.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ThunderToolBarSlot(
+    navigationIcon: @Composable () -> Unit = {},
+    title: @Composable () -> Unit = {},
+    action: @Composable () -> Unit = {}
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            navigationIcon()
+            title()
+        }
+        action()
+    }
+}
