ea9d5bb06557f537074d7c0c9af5a346d0450202
KWY <wodrk1001@gmail.com>
2023-01-13 00:26:01 +0900

    [feat] 메인뷰 상단 구현

--- app/src/main/java/com/koreatech/thunder/feature/thunder/ThunderScreen.kt
+++ app/src/main/java/com/koreatech/thunder/feature/thunder/ThunderScreen.kt
@@ -1,23 +1,53 @@
 package com.koreatech.thunder.feature.thunder
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.koreatech.thunder.R
+import com.koreatech.thunder.designsystem.components.ThunderToolBarSlot
+import com.koreatech.thunder.designsystem.components.thunderChips
+import com.koreatech.thunder.feature.thunder.model.previewHashtagUis
+
+@Preview(showBackground = true)
+@Composable
+fun pv() {
+    ThunderScreen(navController = rememberNavController())
+}
 
 @Composable
 fun ThunderScreen(
     navController: NavController
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    LazyColumn(
+        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        Text(text = "Thunder")
+        item {
+            ThunderToolBarSlot(
+                title = { Text(text = "HanBun") },
+                action = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_thunder),
+                        contentDescription = ""
+                    )
+                }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "진행중인 번개")
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+        thunderChips(previewHashtagUis)
     }
 }
