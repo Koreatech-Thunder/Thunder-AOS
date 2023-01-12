fc5637da9e458f6b179f25b20f420b4ee39b6b85
KWY <wodrk1001@gmail.com>
2023-01-12 23:41:51 +0900

    [feat] thunder chip 구현

---
+++ app/src/main/java/com/koreatech/thunder/designsystem/components/ThunderChip.kt
@@ -0,0 +1,32 @@
+package com.koreatech.thunder.designsystem.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.koreatech.thunder.designsystem.style.Orange
+import com.koreatech.thunder.designsystem.style.Orange200
+import com.koreatech.thunder.feature.thunder.model.HashtagUi
+
+@Composable
+fun ThunderChip(
+    hashtagUi: HashtagUi
+) {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(30.dp))
+            .background(color = if (hashtagUi.isSelected) Orange else Orange200)
+            .padding(vertical = 8.dp, horizontal = 12.dp)
+    ) {
+        Text(
+            text = hashtagUi.hashtag.name,
+            color = Color.White
+        )
+    }
+}
--- app/src/main/java/com/koreatech/thunder/designsystem/style/Color.kt
+++ app/src/main/java/com/koreatech/thunder/designsystem/style/Color.kt
@@ -3,3 +3,4 @@ package com.koreatech.thunder.designsystem.style
 import androidx.compose.ui.graphics.Color
 
 val Orange = Color(0xFFFF7F00)
+val Orange200 = Color(0xFFFFC58B)
