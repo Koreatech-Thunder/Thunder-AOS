375024d757e44886debad70280804921071447d8
KWY <wodrk1001@gmail.com>
2023-01-13 00:25:37 +0900

    [feat] 해시태그 list 구현

---
+++ app/src/main/java/com/koreatech/thunder/designsystem/components/ThunderChips.kt
@@ -0,0 +1,59 @@
+package com.koreatech.thunder.designsystem.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.koreatech.thunder.feature.thunder.model.HashtagUi
+
+fun LazyListScope.thunderChips(
+    hashtags: List<HashtagUi>,
+    columnCount: Int = 4,
+    itemDistance: Dp = 10.dp
+) {
+    gridItems(
+        data = hashtags,
+        columnCount = columnCount,
+        modifier = Modifier,
+        horizontalArrangement = Arrangement.spacedBy(itemDistance)
+    ) { _, hashtag ->
+        ThunderChip(hashtagUi = hashtag)
+    }
+}
+
+private fun <T> LazyListScope.gridItems(
+    data: List<T>,
+    columnCount: Int,
+    modifier: Modifier,
+    horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
+    itemContent: @Composable BoxScope.(index: Int, T) -> Unit
+) {
+    val size = data.count()
+    val rows = if (size == 0) 0 else 1 + (size - 1) / columnCount
+    items(rows, key = { it.hashCode() }) { rowIndex ->
+        Row(
+            horizontalArrangement = horizontalArrangement,
+            modifier = modifier
+        ) {
+            for (columnIndex in 0 until columnCount) {
+                val itemIndex = rowIndex * columnCount + columnIndex
+                if (itemIndex < size) {
+                    Box(
+                        modifier = Modifier.weight(1F, fill = true),
+                        propagateMinConstraints = true
+                    ) {
+                        itemContent(itemIndex, data[itemIndex])
+                    }
+                } else {
+                    Spacer(Modifier.weight(1F, fill = true))
+                }
+            }
+        }
+    }
+}
