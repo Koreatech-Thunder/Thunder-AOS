f77b6cd06e730073f156714b6b0dd44afd7920c5
KWY <wodrk1001@gmail.com>
2023-01-14 00:09:39 +0900

    [feat] hanbun 대표 tool bar 구현

--- app/src/main/java/com/koreatech/thunder/designsystem/components/ThunderToolBarSlot.kt
+++ app/src/main/java/com/koreatech/thunder/designsystem/components/ThunderToolBarSlot.kt
@@ -2,8 +2,6 @@ package com.koreatech.thunder.designsystem.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -11,24 +9,24 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ThunderToolBarSlot(
+    modifier: Modifier,
     navigationIcon: @Composable () -> Unit = {},
     title: @Composable () -> Unit = {},
     action: @Composable () -> Unit = {}
 ) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            navigationIcon()
-            title()
+    ThunderRowSpaceBetweenSlot(
+        modifier = modifier,
+        prefixComponent = {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                navigationIcon()
+                title()
+            }
+        },
+        postfixComponent = {
+            action()
         }
-        action()
-    }
+    )
 }
