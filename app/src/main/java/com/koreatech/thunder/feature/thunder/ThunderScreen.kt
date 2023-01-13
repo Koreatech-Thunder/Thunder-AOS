a9c38f6ae2cfe502007c7c913b479e304d8c1e72
KWY <wodrk1001@gmail.com>
2023-01-14 00:11:48 +0900

    [ui] add thunder screen components

--- app/src/main/java/com/koreatech/thunder/feature/thunder/ThunderScreen.kt
+++ app/src/main/java/com/koreatech/thunder/feature/thunder/ThunderScreen.kt
@@ -1,28 +1,34 @@
 package com.koreatech.thunder.feature.thunder
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.koreatech.thunder.R
+import com.koreatech.thunder.designsystem.components.BlankSpace
+import com.koreatech.thunder.designsystem.components.ThunderChips
 import com.koreatech.thunder.designsystem.components.ThunderToolBarSlot
-import com.koreatech.thunder.designsystem.components.thunderChips
+import com.koreatech.thunder.feature.thunder.components.ThunderItem
 import com.koreatech.thunder.feature.thunder.model.previewHashtagUis
+import com.koreatech.thunder.feature.thunder.model.previewThunderUis
 
 @Preview(showBackground = true)
 @Composable
-fun pv() {
+private fun ThunderScreenPreview() {
     ThunderScreen(navController = rememberNavController())
 }
 
@@ -30,24 +36,38 @@ fun pv() {
 fun ThunderScreen(
     navController: NavController
 ) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        item {
-            ThunderToolBarSlot(
-                title = { Text(text = "HanBun") },
-                action = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_thunder),
-                        contentDescription = ""
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "진행중인 번개")
-            Spacer(modifier = Modifier.height(4.dp))
+    Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)) {
+        ThunderToolBarSlot(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 20.dp),
+            title = {
+                Text(
+                    text = stringResource(R.string.thunder_title),
+                    style = MaterialTheme.typography.h5
+                )
+            },
+            action = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_notifications),
+                    contentDescription = ""
+                )
+            }
+        )
+        Text(
+            text = stringResource(R.string.entering_thunder),
+            style = MaterialTheme.typography.h6
+        )
+        BlankSpace(size = 16.dp)
+        ThunderChips(previewHashtagUis)
+        BlankSpace(size = 12.dp)
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(top = 8.dp)
+        ) {
+            items(previewThunderUis) { thunder ->
+                ThunderItem(thunderUi = thunder)
+            }
         }
-        thunderChips(previewHashtagUis)
     }
 }
