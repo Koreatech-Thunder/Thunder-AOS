5687ba7921e8f8f778605abcc4759800385acb9b
KWY <wodrk1001@gmail.com>
2023-01-15 00:59:44 +0900

    [fix] fix thunder item ui

--- app/src/main/java/com/koreatech/thunder/feature/thunder/components/ThunderItem.kt
+++ app/src/main/java/com/koreatech/thunder/feature/thunder/components/ThunderItem.kt
@@ -21,6 +21,7 @@ import com.koreatech.thunder.designsystem.components.BlankSpace
 import com.koreatech.thunder.designsystem.components.ThunderRowSpaceBetweenSlot
 import com.koreatech.thunder.designsystem.style.Gray200
 import com.koreatech.thunder.designsystem.style.Orange
+import com.koreatech.thunder.designsystem.style.Orange200
 import com.koreatech.thunder.designsystem.style.ThunderTheme
 import com.koreatech.thunder.feature.thunder.model.ThunderUi
 import com.koreatech.thunder.feature.thunder.model.previewThunderUis
@@ -29,54 +30,62 @@ import com.koreatech.thunder.feature.thunder.model.previewThunderUis
 fun ThunderItem(
     thunderUi: ThunderUi
 ) {
-    Column(
-        modifier = Modifier
-            .clip(RoundedCornerShape(8.dp))
-            .background(color = Gray200)
-            .padding(horizontal = 16.dp, vertical = 12.dp)
-    ) {
-        Text(
-            text = thunderUi.deadline,
-            style = ThunderTheme.typography.h5,
-            color = Orange
-        )
-        BlankSpace(10.dp)
-        ThunderTextHashtags(thunderUi.hashtags)
-        BlankSpace(8.dp)
-        Text(
-            text = thunderUi.title,
-            style = ThunderTheme.typography.h4
-        )
-        BlankSpace(10.dp)
-        Text(
-            text = thunderUi.content,
-            style = ThunderTheme.typography.h5
-        )
-        BlankSpace(12.dp)
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.End
+    Column {
+        Column(
+            modifier = Modifier
+                .clip(RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp))
+                .background(color = Gray200)
+                .padding(horizontal = 16.dp, vertical = 10.dp)
         ) {
-            ThunderButton(thunderUi.thunderState)
-        }
-        BlankSpace(20.dp)
-        ThunderRowSpaceBetweenSlot(
-            prefixComponent = {
-                Text(
-                    text = stringResource(R.string.participants),
-                    style = ThunderTheme.typography.h5
-                )
-            },
-            postfixComponent = {
-                ThunderParticipantsDetailSection(
-                    thunderUi.participants.size,
-                    thunderUi.limitParticipantsCnt
-                )
+            Text(
+                text = thunderUi.deadline,
+                style = ThunderTheme.typography.h5,
+                color = Orange
+            )
+            BlankSpace(10.dp)
+            ThunderTextHashtags(thunderUi.hashtags)
+            BlankSpace(8.dp)
+            Text(
+                text = thunderUi.title,
+                style = ThunderTheme.typography.h4
+            )
+            BlankSpace(10.dp)
+            Text(
+                text = thunderUi.content,
+                style = ThunderTheme.typography.h5
+            )
+            BlankSpace(12.dp)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                ThunderButton(thunderUi.thunderState)
             }
-        )
-        BlankSpace(8.dp)
-        Image(painter = painterResource(id = R.drawable.ic_school), contentDescription = "")
-        ThunderParticipants(thunderUi.participants)
+        }
+        Column(
+            modifier = Modifier
+                .clip(RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp))
+                .background(color = Orange200)
+                .padding(horizontal = 16.dp, vertical = 10.dp)
+        ) {
+            ThunderRowSpaceBetweenSlot(
+                prefixComponent = {
+                    Text(
+                        text = stringResource(R.string.participants),
+                        style = ThunderTheme.typography.h5
+                    )
+                },
+                postfixComponent = {
+                    ThunderParticipantsDetailSection(
+                        thunderUi.participants.size,
+                        thunderUi.limitParticipantsCnt
+                    )
+                }
+            )
+            BlankSpace(8.dp)
+            Image(painter = painterResource(id = R.drawable.ic_school), contentDescription = "")
+            ThunderParticipants(thunderUi.participants)
+        }
     }
 }
 
