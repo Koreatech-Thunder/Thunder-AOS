a2df0611d3756d46782815c6f39629600eff7771
KWY <wodrk1001@gmail.com>
2023-01-15 00:52:26 +0900

    [feat] create host hat

--- app/src/main/java/com/koreatech/thunder/feature/thunder/components/ThunderItem.kt
+++ app/src/main/java/com/koreatech/thunder/feature/thunder/components/ThunderItem.kt
@@ -1,5 +1,6 @@
 package com.koreatech.thunder.feature.thunder.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -73,6 +75,7 @@ fun ThunderItem(
             }
         )
         BlankSpace(8.dp)
+        Image(painter = painterResource(id = R.drawable.ic_school), contentDescription = "")
         ThunderParticipants(thunderUi.participants)
     }
 }
