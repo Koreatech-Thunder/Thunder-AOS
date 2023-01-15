c95bedc2b5b8c2533fe5708e5d7688c9b62d6b73
KWY <wodrk1001@gmail.com>
2023-01-15 15:53:14 +0900

    [fix] bottom navigation item color change orange to white

--- app/src/main/java/com/koreatech/thunder/navigation/ThunderBottomBar.kt
+++ app/src/main/java/com/koreatech/thunder/navigation/ThunderBottomBar.kt
@@ -1,12 +1,14 @@
 package com.koreatech.thunder.navigation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
@@ -14,6 +16,7 @@ import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import com.koreatech.thunder.designsystem.style.Orange
+import com.koreatech.thunder.designsystem.style.Orange200
 
 @Composable
 fun RowScope.ThunderBottomBar(
@@ -23,12 +26,23 @@ fun RowScope.ThunderBottomBar(
 ) {
     destinations.forEach { screen ->
         BottomNavigationItem(
-            modifier = Modifier.background(Orange),
-            label = { Text(text = stringResource(id = screen.title)) },
+            modifier = Modifier
+                .background(Color.White),
+            label = {
+                Text(
+                    text = stringResource(id = screen.title),
+                    color = if (currentDestination?.hierarchy?.any { it.route == screen.name } == true) Orange else Orange200
+                )
+            },
             icon = {
-                Icon(
+                Image(
                     painter = painterResource(id = screen.icon),
-                    contentDescription = stringResource(id = screen.description)
+                    contentDescription = stringResource(id = screen.description),
+                    colorFilter = if (currentDestination?.hierarchy?.any { it.route == screen.name } == true) {
+                        ColorFilter.tint(Orange)
+                    } else {
+                        ColorFilter.tint(Orange200)
+                    }
                 )
             },
             selected = currentDestination?.hierarchy?.any { it.route == screen.name } == true,
