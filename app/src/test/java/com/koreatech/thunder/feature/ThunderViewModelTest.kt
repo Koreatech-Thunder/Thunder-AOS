e160835e4e6548be6b3db9ce3f05ca13437c9673
KWY <wodrk1001@gmail.com>
2023-01-22 03:00:37 +0900

    [fix] del collect job

--- app/src/test/java/com/koreatech/thunder/feature/ThunderViewModelTest.kt
+++ app/src/test/java/com/koreatech/thunder/feature/ThunderViewModelTest.kt
@@ -10,9 +10,6 @@ import io.mockk.mockk
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
@@ -35,8 +32,6 @@ class ThunderViewModelTest {
     @DisplayName("사용자가 하나 이상 해시태그를 설정했고 통신을 성공했다면 사용자의 해시태그를 보여준다")
     @Test
     fun hashtagTest() = runTest {
-        val collectJob =
-            launch(UnconfinedTestDispatcher()) { thunderViewModel.hashtagUiState.collect() }
         val expectedHashtags = listOf(Hashtag.SPORT, Hashtag.MOVIE, Hashtag.WALK)
         coEvery { thunderRepository.getHashtags() } returns Result.success(expectedHashtags)
 
@@ -46,21 +41,18 @@ class ThunderViewModelTest {
         val data = thunderViewModel.hashtagUiState.value
         assertIs<HashtagUiState.Success>(data)
         assertEquals(data.hashtags, expectedHashtags)
-        collectJob.cancel()
     }
 
     @DisplayName("사용자가 해시태그를 설정하지 않았으면 모든 해시태그를 보여준다.")
     @Test
     fun hashtagTest2() = runTest {
-        val collectJob = launch { thunderViewModel.hashtagUiState.collect() }
         val expectedHashtags = Hashtag.values().toList()
         coEvery { thunderRepository.getHashtags() } returns Result.success(emptyList())
 
         thunderViewModel.getHashtags()
         val data = thunderViewModel.hashtagUiState.value
-        
+
         assertIs<HashtagUiState.Success>(data)
         Assertions.assertEquals(data.hashtags, expectedHashtags)
-        collectJob.cancel()
     }
 }
