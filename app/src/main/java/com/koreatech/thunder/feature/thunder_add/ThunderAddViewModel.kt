7c901a186120ae971a6eb9081cf28878a92eab1a
KWY <wodrk1001@gmail.com>
2023-01-23 13:57:55 +0900

    [feat] success test code

--- app/src/main/java/com/koreatech/thunder/feature/thunder_add/ThunderAddViewModel.kt
+++ app/src/main/java/com/koreatech/thunder/feature/thunder_add/ThunderAddViewModel.kt
@@ -1,4 +1,20 @@
 package com.koreatech.thunder.feature.thunder_add
 
-class ThunderAddViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ThunderAddViewModel : ViewModel() {
+    private val _limitParticipantsCnt = MutableStateFlow(2)
+    val limitParticipantsCnt = _limitParticipantsCnt.asStateFlow()
+
+    fun plusLimitParticipantsCnt() {
+        _limitParticipantsCnt.value = _limitParticipantsCnt.value + 1
+    }
+
+    fun minusLimitParticipantsCnt() {
+        if (limitParticipantsCnt.value > 2) {
+            _limitParticipantsCnt.value = _limitParticipantsCnt.value - 1
+        }
+    }
 }
