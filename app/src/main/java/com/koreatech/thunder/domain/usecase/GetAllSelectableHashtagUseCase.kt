55e584fe534a19da8e9b51e2871c753d0a2e62a1
KWY <wodrk1001@gmail.com>
2023-01-23 14:30:28 +0900

    [feat] success usecase test code

--- app/src/main/java/com/koreatech/thunder/domain/usecase/GetAllSelectableHashtagUseCase.kt
+++ app/src/main/java/com/koreatech/thunder/domain/usecase/GetAllSelectableHashtagUseCase.kt
@@ -1,4 +1,12 @@
 package com.koreatech.thunder.domain.usecase
 
-class GetAllSelectableHashtagUseCase {
+import com.koreatech.thunder.domain.model.Hashtag
+import com.koreatech.thunder.domain.model.SelectableHashtag
+import javax.inject.Inject
+
+class GetAllSelectableHashtagUseCase @Inject constructor() {
+    operator fun invoke(selectable: Boolean = false): List<SelectableHashtag> =
+        Hashtag.values().map { hashtag ->
+            SelectableHashtag(hashtag, selectable)
+        }
 }
