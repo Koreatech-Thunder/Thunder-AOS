455a87f84e531e13547ff0e8a5ad8347c5b91b79
KWY <wodrk1001@gmail.com>
2023-01-24 16:37:27 +0900

    [feat] success hashtag test code

--- app/src/main/java/com/koreatech/thunder/feature/thunder_add/ThunderAddViewModel.kt
+++ app/src/main/java/com/koreatech/thunder/feature/thunder_add/ThunderAddViewModel.kt
@@ -1,12 +1,25 @@
 package com.koreatech.thunder.feature.thunder_add
 
 import androidx.lifecycle.ViewModel
+import com.koreatech.thunder.domain.model.SelectableHashtag
+import com.koreatech.thunder.domain.usecase.GetAllSelectableHashtagUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
-class ThunderAddViewModel : ViewModel() {
+@HiltViewModel
+class ThunderAddViewModel @Inject constructor(
+    private val getAllSelectableHashtagUseCase: GetAllSelectableHashtagUseCase
+) : ViewModel() {
+    private val _hashtags: MutableStateFlow<List<SelectableHashtag>> = MutableStateFlow(emptyList())
     private val _limitParticipantsCnt = MutableStateFlow(2)
     val limitParticipantsCnt = _limitParticipantsCnt.asStateFlow()
+    val hashtags = _hashtags.asStateFlow()
+
+    init {
+        _hashtags.value = getAllSelectableHashtagUseCase()
+    }
 
     fun plusLimitParticipantsCnt() {
         _limitParticipantsCnt.value = _limitParticipantsCnt.value + 1
