73b81ff8736287508d62449478de34ec91d1bc90
KWY <wodrk1001@gmail.com>
2023-01-24 16:37:14 +0900

    [test] create hashtag test code

--- app/src/test/java/com/koreatech/thunder/feature/ThunderAddViewModelTest.kt
+++ app/src/test/java/com/koreatech/thunder/feature/ThunderAddViewModelTest.kt
@@ -1,5 +1,8 @@
 package com.koreatech.thunder.feature
 
+import com.koreatech.thunder.domain.model.Hashtag
+import com.koreatech.thunder.domain.model.SelectableHashtag
+import com.koreatech.thunder.domain.usecase.GetAllSelectableHashtagUseCase
 import com.koreatech.thunder.feature.thunder_add.ThunderAddViewModel
 import kotlin.test.assertEquals
 import org.junit.jupiter.api.BeforeEach
@@ -7,11 +10,12 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class ThunderAddViewModelTest {
+    private val getAllSelectableHashtagUseCase = GetAllSelectableHashtagUseCase()
     private lateinit var thunderAddViewModel: ThunderAddViewModel
 
     @BeforeEach
     fun setUp() {
-        thunderAddViewModel = ThunderAddViewModel()
+        thunderAddViewModel = ThunderAddViewModel(getAllSelectableHashtagUseCase)
     }
 
     @DisplayName("제한 인원은 2명으로 초기화 되어있다.")
@@ -56,5 +60,16 @@ class ThunderAddViewModelTest {
         thunderAddViewModel.minusLimitParticipantsCnt()
         count = thunderAddViewModel.limitParticipantsCnt.value
         assertEquals(count, 2)
+    }
+
+    @DisplayName("첫 진입 시 모든 해시태그가 들어있고, false 상태이다.")
+    @Test
+    fun hashtagTest() {
+        val hashtags: List<SelectableHashtag> = thunderAddViewModel.hashtags.value
+
+        assertEquals(hashtags.size, Hashtag.values().size)
+        hashtags.forEach { hashtag ->
+            assertEquals(hashtag.isSelected, false)
+        }
     }
 }
