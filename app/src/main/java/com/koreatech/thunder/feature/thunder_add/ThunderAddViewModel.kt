962fbe7c09785634c635ad67c279d96f0c183dd4
KWY <wodrk1001@gmail.com>
2023-01-24 17:05:37 +0900

    [feat] success test code

--- app/src/main/java/com/koreatech/thunder/feature/thunder_add/ThunderAddViewModel.kt
+++ app/src/main/java/com/koreatech/thunder/feature/thunder_add/ThunderAddViewModel.kt
@@ -4,14 +4,15 @@ import androidx.lifecycle.ViewModel
 import com.koreatech.thunder.domain.model.SelectableHashtag
 import com.koreatech.thunder.domain.usecase.GetAllSelectableHashtagUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
 @HiltViewModel
 class ThunderAddViewModel @Inject constructor(
     private val getAllSelectableHashtagUseCase: GetAllSelectableHashtagUseCase
 ) : ViewModel() {
+    private var selectedHashtagCount = 0
     private val _hashtags: MutableStateFlow<List<SelectableHashtag>> = MutableStateFlow(emptyList())
     private val _limitParticipantsCnt = MutableStateFlow(2)
     val limitParticipantsCnt = _limitParticipantsCnt.asStateFlow()
@@ -32,11 +33,20 @@ class ThunderAddViewModel @Inject constructor(
     }
 
     fun selectHashtag(selectIndex: Int) {
-        val afterHashtags =
-            hashtags.value.mapIndexed { index, selectableHashtag ->
-                if (index == selectIndex) selectableHashtag.copy(isSelected = !selectableHashtag.isSelected)
-                else selectableHashtag
-            }
-        _hashtags.value = afterHashtags
+        if (selectedHashtagCount < SELECTABLE_HASHTAG_COUNT || hashtags.value[selectIndex].isSelected) {
+            val afterHashtags =
+                hashtags.value.mapIndexed { index, selectableHashtag ->
+                    if (index == selectIndex) {
+                        if (selectableHashtag.isSelected) selectedHashtagCount--
+                        else selectedHashtagCount++
+                        selectableHashtag.copy(isSelected = !selectableHashtag.isSelected)
+                    } else selectableHashtag
+                }
+            _hashtags.value = afterHashtags
+        }
+    }
+
+    companion object {
+        const val SELECTABLE_HASHTAG_COUNT = 4
     }
 }
