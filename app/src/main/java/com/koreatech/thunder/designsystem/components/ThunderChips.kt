8bd39f396bc535e796a10b4476eb08aef70cd3d2
KWY <wodrk1001@gmail.com>
2023-01-31 00:05:18 +0900

    [ui] fix lazygridcolum to custom grid

--- app/src/main/java/com/koreatech/thunder/designsystem/components/ThunderChips.kt
+++ app/src/main/java/com/koreatech/thunder/designsystem/components/ThunderChips.kt
@@ -1,10 +1,12 @@
 package com.koreatech.thunder.designsystem.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.koreatech.thunder.domain.model.SelectableHashtag
 
@@ -12,20 +14,37 @@ import com.koreatech.thunder.domain.model.SelectableHashtag
 fun ThunderChips(
     selectableHashtags: List<SelectableHashtag>,
     isClickable: Boolean = false,
+    spanCount: Int = 4,
     selectHashtag: (Int) -> Unit = {}
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(4),
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
+    val size = selectableHashtags.size
+    val columnCount = if (size == 0) 0 else 1 + (size - 1) / spanCount
+    Column(
         verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
-        itemsIndexed(selectableHashtags) { idx, hashtag ->
-            ThunderChip(
-                index = idx,
-                selectableHashtag = hashtag,
-                isClickable = isClickable,
-                selectHashtag = selectHashtag
-            )
+        repeat(columnCount) { colIndex ->
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                repeat(spanCount) { rowIndex ->
+                    val itemIndex = spanCount * colIndex + rowIndex
+                    if (itemIndex < size) {
+                        Box(
+                            modifier = Modifier.weight(1f, fill = true),
+                            propagateMinConstraints = true
+                        ) {
+                            ThunderChip(
+                                index = itemIndex,
+                                selectableHashtag = selectableHashtags[itemIndex],
+                                isClickable = isClickable,
+                                selectHashtag = selectHashtag
+                            )
+                        }
+                    } else {
+                        Spacer(Modifier.weight(1F, fill = true))
+                    }
+                }
+            }
         }
     }
 }
