c08f7dfed22d51ec86fabd1ead3b335aac7155e8
KWY <wodrk1001@gmail.com>
2023-01-30 16:59:35 +0900

    [ui] create report dialog

---
+++ app/src/main/java/com/koreatech/thunder/feature/thunder/components/ReportDialog.kt
@@ -0,0 +1,69 @@
+package com.koreatech.thunder.feature.thunder.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.koreatech.thunder.R
+import com.koreatech.thunder.designsystem.components.ThunderDialogSlot
+import com.koreatech.thunder.designsystem.style.ThunderTheme
+
+@Composable
+fun ReportDialog(
+    onDismissRequest: () -> Unit
+) {
+    ThunderDialogSlot(
+        radius = 15.dp,
+        onDismissRequest = onDismissRequest
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = 16.dp,
+                    vertical = 24.dp
+                ),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.thunder_report_dialog_title),
+                style = ThunderTheme.typography.h2
+            )
+            Text(
+                text = stringResource(R.string.thunder_report_dialog_content),
+                style = ThunderTheme.typography.h5
+            )
+            Text(
+                modifier = Modifier.clickable { },
+                text = stringResource(R.string.thunder_report_1),
+                style = ThunderTheme.typography.b3
+            )
+            Text(
+                modifier = Modifier.clickable { },
+                text = stringResource(R.string.thunder_report_2),
+                style = ThunderTheme.typography.b3
+            )
+            Text(
+                modifier = Modifier.clickable { },
+                text = stringResource(R.string.thunder_report_3),
+                style = ThunderTheme.typography.b3
+            )
+            Text(
+                modifier = Modifier.clickable { },
+                text = stringResource(R.string.thunder_report_4),
+                style = ThunderTheme.typography.b3
+            )
+            Text(
+                modifier = Modifier.clickable { },
+                text = stringResource(R.string.thunder_report_5),
+                style = ThunderTheme.typography.b3
+            )
+        }
+    }
+}
