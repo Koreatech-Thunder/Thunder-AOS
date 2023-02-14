8bb953ebfbef110a3e4040d422b4b450c684770e
KWY <wodrk1001@gmail.com>
2023-02-14 22:27:46 +0900

    [ui] create temperate login ui

--- app/src/main/java/com/koreatech/thunder/designsystem/style/Color.kt
+++ app/src/main/java/com/koreatech/thunder/designsystem/style/Color.kt
@@ -4,5 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val Orange = Color(0xFFFF7F00)
 val Orange200 = Color(0xFFFFC58B)
+val KakaoYellow = Color(0xFFFEE400)
+val KakaoBrown = Color(0xFF4E2A00)
 val Gray200 = Color(0xFFFAFAFA)
 val Gray = Color(0xFFD6D6D6)
--- app/src/main/java/com/koreatech/thunder/feature/onboarding/LoginScreen.kt
+++ app/src/main/java/com/koreatech/thunder/feature/onboarding/LoginScreen.kt
@@ -1,10 +1,54 @@
 package com.koreatech.thunder.feature.onboarding
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.koreatech.thunder.R
+import com.koreatech.thunder.designsystem.style.KakaoBrown
+import com.koreatech.thunder.designsystem.style.KakaoYellow
+import com.koreatech.thunder.designsystem.style.ThunderTheme
 
 @Composable
 fun LoginScreen() {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = stringResource(R.string.app_name),
+            style = ThunderTheme.typography.h1
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, bottom = 24.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(KakaoYellow)
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp),
+                text = stringResource(R.string.kakao_login),
+                color = KakaoBrown,
+                style = ThunderTheme.typography.b2,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
