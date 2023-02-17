0aa19abd4d7c01bcd81a884d2221af52e7b6344e
KWY <wodrk1001@gmail.com>
2023-02-17 15:10:09 +0900

    HAN-42 [test] create test code

---
+++ app/src/test/java/com/koreatech/thunder/feature/ProfileViewModelTest.kt
@@ -0,0 +1,44 @@
+package com.koreatech.thunder.feature
+
+import com.koreatech.thunder.domain.model.dummyUsers
+import com.koreatech.thunder.domain.repository.UserRepository
+import com.koreatech.thunder.domain.usecase.GetUserProfileUseCase
+import com.koreatech.thunder.feature.profile.ProfileViewModel
+import com.koreatech.thunder.util.CoroutinesTestExtension
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlin.test.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@ExtendWith(CoroutinesTestExtension::class)
+class ProfileViewModelTest {
+    private val userRepository: UserRepository = mockk()
+    private val getUserProfileUseCase = GetUserProfileUseCase(userRepository)
+    private lateinit var profileViewModel: ProfileViewModel
+
+    @BeforeEach
+    fun setUp() {
+        profileViewModel = ProfileViewModel(
+            getUserProfileUseCase
+        )
+    }
+
+    @DisplayName("프로필 뷰 진입 시 유저의 프로필 정보를 불러온다.")
+    @Test
+    fun profileTest() = runTest {
+        coEvery { userRepository.getUserProfile() } returns Result.success(dummyUsers[0])
+
+        profileViewModel.getUserProfile()
+
+        assertEquals("KWY", profileViewModel.user.value.name)
+        assertEquals("컴퓨터 공학부", profileViewModel.user.value.introduction)
+        assertEquals(36, profileViewModel.user.value.temperature)
+        assertEquals(2, profileViewModel.user.value.hashtags.size)
+    }
+}
