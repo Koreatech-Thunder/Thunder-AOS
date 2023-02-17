5774f3d03c1c6f0757cdecfda1619b28e3988572
KWY <wodrk1001@gmail.com>
2023-02-17 17:40:55 +0900

    HAN-45 [feat] success test code

--- app/src/main/java/com/koreatech/thunder/feature/profile/AlarmSettingViewModel.kt
+++ app/src/main/java/com/koreatech/thunder/feature/profile/AlarmSettingViewModel.kt
@@ -1,4 +1,21 @@
 package com.koreatech.thunder.feature.profile
 
-class AlarmSettingViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+@HiltViewModel
+class AlarmSettingViewModel @Inject constructor() : ViewModel() {
+    private val _alarms: MutableStateFlow<List<Boolean>> =
+        MutableStateFlow(listOf(false, false, false))
+    val alarms = _alarms.asStateFlow()
+
+    fun clickAlarm(index: Int) {
+        _alarms.value = alarms.value.mapIndexed { idx, isChecked ->
+            if (idx == index) !isChecked
+            else isChecked
+        }
+    }
 }
