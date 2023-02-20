8f99feacd672b03fe236f91b0eac6e7f41a58fe5
KWY <wodrk1001@gmail.com>
2023-02-20 19:08:42 +0900

    HAN-38 [feat] basic network setting

---
+++ app/src/main/java/com/koreatech/thunder/di/RetrofitModule.kt
@@ -0,0 +1,63 @@
+package com.koreatech.thunder.di
+
+import com.google.gson.GsonBuilder
+import com.koreatech.thunder.data.source.local.AuthLocalDataSource
+import dagger.Provides
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitModule {
+    @Provides
+    @Singleton
+    fun providesInterceptor(
+        authLocalDataSource: AuthLocalDataSource
+    ): Interceptor =
+        Interceptor { chain ->
+            with(chain) {
+                proceed(
+                    request()
+                        .newBuilder()
+                        .addHeader(
+                            name = "Authorization",
+                            value = "Bearer" + authLocalDataSource.accessToken
+                        )
+                        .build()
+                )
+            }
+        }
+
+    @Provides
+    @Singleton
+    fun providesThunderOkHttpClient(
+        interceptor: Interceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .writeTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.SECONDS)
+            .addInterceptor(interceptor)
+            .addInterceptor(
+                HttpLoggingInterceptor().also { it.level = HttpLoggingInterceptor.Level.BODY }
+            )
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesThunderRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("THUNDER_URL")
+            .client(okHttpClient)
+            .addConverterFactory(
+                GsonConverterFactory.create(
+                    GsonBuilder().serializeNulls().create()
+                )
+            )
+            .build()
+}
