7cc94dfe116e3af0b3fc9c43fa7f577d6dce77f5
KWY <wodrk1001@gmail.com>
2023-03-14 19:51:43 +0900

    [chore] add dummy data in record thunder

--- app/src/main/java/com/koreatech/thunder/feature/profile/ThunderRecordViewModel.kt
+++ app/src/main/java/com/koreatech/thunder/feature/profile/ThunderRecordViewModel.kt
@@ -2,11 +2,12 @@ package com.koreatech.thunder.feature.profile
 
 import androidx.lifecycle.ViewModel
 import com.koreatech.thunder.domain.model.Thunder
+import com.koreatech.thunder.domain.model.dummyThunders
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class ThunderRecordViewModel @Inject constructor() : ViewModel() {
-    private val _thunderRecord: MutableStateFlow<List<Thunder>> = MutableStateFlow(emptyList())
+    private val _thunderRecord: MutableStateFlow<List<Thunder>> = MutableStateFlow(dummyThunders)
     val thunderRecord = _thunderRecord.asStateFlow()
 }
