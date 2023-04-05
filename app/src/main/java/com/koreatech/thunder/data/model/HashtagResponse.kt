0c349e33152663ac5bbd4a4ee192731c2eb7c092
KWY <wodrk1001@gmail.com>
2023-04-05 23:53:59 +0900

    [chore] move package in response

--- app/src/main/java/com/koreatech/thunder/data/model/HashtagResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.koreatech.thunder.data.model
-
-import com.koreatech.thunder.domain.model.Hashtag
-
-data class HashtagResponse(
-    val hashtags: List<String>
-) {
-    fun toHashtag() = hashtags.forEach { hashtag -> Hashtag.valueOf(hashtag) }
-}
--- app/src/main/java/com/koreatech/thunder/data/model/response/ThunderResponse.kt
+++ app/src/main/java/com/koreatech/thunder/data/model/response/ThunderResponse.kt
@@ -1,4 +1,4 @@
-package com.koreatech.thunder.data.model
+package com.koreatech.thunder.data.model.response
 
 import com.google.gson.annotations.SerializedName
 import com.koreatech.thunder.domain.model.Hashtag
