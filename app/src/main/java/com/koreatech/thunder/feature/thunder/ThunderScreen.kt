6af3fdaa90b92b78bdf1544be00ad4a7a95d5d7c
KWY <wodrk1001@gmail.com>
2023-05-16 23:25:39 +0900

    [chore] delete alarm icon

--- app/src/main/java/com/koreatech/thunder/feature/thunder/ThunderScreen.kt
+++ app/src/main/java/com/koreatech/thunder/feature/thunder/ThunderScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -112,13 +111,8 @@ fun ThunderScreen(
                             text = stringResource(R.string.thunder_title),
                             style = ThunderTheme.typography.h1
                         )
-                    },
-                    action = {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_notifications),
-                            contentDescription = ""
-                        )
-                    })
+                    }
+                )
                 Text(
                     text = stringResource(R.string.entering_thunder),
                     style = ThunderTheme.typography.h3
